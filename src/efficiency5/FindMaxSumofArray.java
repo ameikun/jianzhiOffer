5748f7cbff02bc720785f3b96373fba24ff7ed55
ameikun <13618358620@163.com>
2018-04-22 12:06:46 +0800

    连续子数组的最大和

---
+++ src/efficiency5/FindMaxSumofArray.java
@@ -0,0 +1,33 @@
+package efficiency5;
+
+/**
+ * 连续子数组的最大和*/
+public class FindMaxSumofArray {
+	public int MaxSumofSubArray(int[] array) {
+		if (array.length<=0) {
+			return 0;
+		}
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (sum <= 0)
+				sum = array[i];
+			else
+				sum = sum + array[i];
+			if (max < sum)
+				max = sum;
+		}
+		return max;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FindMaxSumofArray maxSumofArray = new FindMaxSumofArray();
+		int[] array = { 1, -2, 3, 10, -4, 7, 2, -5 };
+		System.out.println(maxSumofArray.MaxSumofSubArray(array));
+	}
+
+}
